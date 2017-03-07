f33f835e7094fc9e7fe73f203c60c8107fb09c49
yswu1022eng <yswu1022eng@gmail.com>
2017-03-07 20:26:37 +0800

    浮點數與round-off errors

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -10,12 +10,9 @@ namespace Example01
     {
         private static void Main(string[] args)
         {
-            Console.Write("請輸入使用者名稱:");
-            string name = Console.ReadLine();
-            Console.Write("請輸入密碼:");
-            string password = Console.ReadLine();
-
-            Console.WriteLine("嗨,{0}, 歡迎回來", name);
+            Console.WriteLine((1 / 2) * 10);
+            Console.WriteLine((1.0 / 2.0) * 10.0);
+            Console.ReadLine();
         }
     }
 }
