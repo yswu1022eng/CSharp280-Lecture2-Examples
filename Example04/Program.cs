d5b9abf278a4948cf4b00f0d78d6840f8f6e268e
yswu1022eng <yswu1022eng@gmail.com>
2017-03-07 21:43:37 +0800

    Example4: 加入錯誤後中斷程式控制

--- Example04/Program.cs
+++ Example04/Program.cs
@@ -12,6 +12,7 @@ namespace Example04
         {
             int number1 = 0;
             int number2 = 0;
+
             Console.Write("請輸入第一個數字:");
             try
             {
@@ -19,7 +20,9 @@ namespace Example04
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine("第一個數字輸入錯誤");
+                Console.WriteLine("錯誤訊息:\n" + e.ToString());
+                return;
             }
 
             Console.Write("請輸入第二個數字:");
@@ -30,7 +33,9 @@ namespace Example04
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine("第二個數字輸入錯誤");
+                Console.WriteLine("錯誤訊息:\n" + e.ToString());
+                return;
             }
             Console.WriteLine("{0} + {1} = {2}", number1, number2, number1 + number2);
 
