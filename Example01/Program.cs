f3d8d5ae31623d3e55e1c65d5c30b85dd8958e2b
yswu1022eng <yswu1022eng@gmail.com>
2017-03-07 19:56:11 +0800

    完成簡單登入程式

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace Example01
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.Write("請輸入使用者名稱:");
+            string name = Console.ReadLine();
+            Console.Write("請輸入密碼:");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("嗨,{0}, 歡迎回來", name);
         }
     }
 }
